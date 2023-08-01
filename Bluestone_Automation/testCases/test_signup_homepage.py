d7f7686bf83e7e9136a6e6c42ac6601a7422178a
afiyabegum <141004345+afiyabegum@users.noreply.github.com>
2023-08-01 12:34:37 +0530

    test_signup_homepage.py

--- Bluestone_Automation/testCases/test_signup_homepage.py
+++ Bluestone_Automation/testCases/test_signup_homepage.py
@@ -6,11 +6,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # Replace 'your_browser_driver_path' with the path to your browser driver executable (e.g., chromedriver, geckodriver)
-driver = webdriver.Chrome(executable_path='your_browser_driver_path')
+driver = webdriver.Chrome(')
 driver.maximize_window()
 
 # Replace 'bluestone_website_url' with the actual URL of Bluestone's signup page
-website_url = 'bluestone_website_url'
+website_url = 'https://www.bluestone.com/'
 driver.get(website_url)
 
 # Wait for the signup page to load
@@ -45,8 +45,6 @@ if 'success' in driver.current_url:
 else:
     print("Signup failed!")
 
-# Close the browser
-driver.quit()
 
 #bluestone homepage
 from selenium import webdriver
@@ -56,11 +54,11 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # Replace 'your_browser_driver_path' with the path to your browser driver executable (e.g., chromedriver, geckodriver)
-driver = webdriver.Chrome(executable_path='your_browser_driver_path')
+driver = webdriver.Chrome('')
 driver.maximize_window()
 
 # Replace 'bluestone_website_url' with the actual URL of Bluestone's homepage
-website_url = 'bluestone_website_url'
+website_url = 'https://www.bluestone.com/'
 driver.get(website_url)
 
 # Wait for the homepage to load
